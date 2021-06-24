8d6acd10f40481c48af1774fc652d141f83628e9
trevf002 <72757928+trevf002@users.noreply.github.com>
2021-06-24 14:21:10 -0700

    update

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -4,4 +4,48 @@ dept_name VARCHAR NOT NULL,
 PRIMARY KEY (dept_no)
 )
 
-SELECT * FROM departments;
+SELECT * FROM departments;CREATE TABLE Titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+SELECT * from Titles
+
+CREATE TABLE Employees (
+	emp_no INTEGER NOT NULL,
+	emp_title VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL, 
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title) REFERENCES Titles (title_id)
+);
+
+SELECT * from Employees
+
+CREATE TABLE DepartmentEmployees (
+	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
+);
+SELECT * from DepartmentEmployees
+
+CREATE TABLE Managers (
+	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
+);
+SELECT * from Managers
+
+CREATE TABLE Salaries (
+	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
+);
+
+SELECT * from Salaries
